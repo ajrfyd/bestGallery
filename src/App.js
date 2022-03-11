66bb559e24c00f0df21c4de6d1f7f47d24d3e34d
ajrfyd <ajrfyd@naver.com>
2022-03-11 17:48:36 +0900

    Test of deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
-
+  
   return (
-    <div>
+    <Container>
       Hello Gallery??
-    </div>
+    </Container>
   )
 }
 
 export default App;
+
+// const col = 'black'
+const h = '30vh'
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: ${h};
+  background-color: #6200ee
+`
